56870c1e1db6b5ac1db518df8817654c25c911ce
JH <jiho4890@gmail.com>
2026-03-19 16:40:25 +0900

    feat: exit 입력 시 종료 기능 구현

--- src/com/jiho/calculator/App.java
+++ src/com/jiho/calculator/App.java
@@ -5,39 +5,52 @@ import java.util.*;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("첫 번째 숫자를 입력하세요.");
-        int n1 = sc.nextInt();
-        System.out.println("두 번째 숫자를 입력하세요.");
-        int n2 = sc.nextInt();
 
-        System.out.println("사칙연산 기호를 입력하세요: ");
-        char Operator = sc.next().charAt(0);
+        boolean exit = true;
 
-        int result = 0;
-        switch (Operator) {
-            case '+' :
-                result = n1 + n2;
-                break;
-            case '-' :
-                result = n1 - n2;
-                break;
-            case '*' :
-                result = n1 * n2;
-                break;
-            case '/' :
-                if(n2 != 0){
-                    result = n1 / n2;
-                } else{
-                    System.out.println("니눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    return;
-                }
-                break;
-            default:
-                System.out.println("알맞은 사칙연산 기호를 입력해주세요.");
-                return;
+        while (exit) {
+            System.out.println("첫 번째 숫자를 입력하세요.");
+            int n1 = sc.nextInt();
+
+            System.out.println("두 번째 숫자를 입력하세요.");
+            int n2 = sc.nextInt();
+
+            System.out.println("사칙연산 기호를 입력하세요: ");
+            char Operator = sc.next().charAt(0);
+
+            int result = 0;
+
+            switch (Operator) {
+                case '+' :
+                    result = n1 + n2;
+                    break;
+                case '-' :
+                    result = n1 - n2;
+                    break;
+                case '*' :
+                    result = n1 * n2;
+                    break;
+                case '/' :
+                    if(n2 != 0){
+                        result = n1 / n2;
+                    } else{
+                        System.out.println("니눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                        break;
+                    }
+                    break;
+                default:
+                    System.out.println("알맞은 사칙연산 기호를 입력해주세요.");
+                    break;
+            }
+
+            System.out.println("결과: " + result);
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시만 종료)");
+            String choose = sc.next();
+            if (choose.equals("exit")){
+                exit = false;
+            }
         }
-
-        System.out.println("결과: " + result);
 
     }
 }
